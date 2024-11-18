4fd3069adb9d3c5b314f424aa096c07ccf16cabe
Gopal Mahto <gopal02mah@gmail.com>
2024-11-18 17:07:05 +0000

    conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score=undefined;
+console.log(typeof score);
+console.log(score);
+
+let val = Number(score);
+console.log(typeof val);
+console.log(val);
+
+
+//"33" => 33
+//null => NaN
+//true => 1  false => 0
+//undefined => 0
+
+let loggedIn = "sg";
+let isLoggedIn = Boolean(loggedIn);
+console.log(isLoggedIn);
+
+// 1 =>true ; 0 => false
+// "" =>false 
+// "sg" =>true
+
+console.log("---------------");
+
+
+let value = 45;
+let str = String(value)
+console.log(str);
+console.log(typeof str);
